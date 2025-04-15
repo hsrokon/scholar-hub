d5e5fe573f932742df32a0578c36ef86e924673a
hsrokon <hsrokonofficial@gmail.com>
2025-04-15 23:47:34 +0600

    logo added with favicon, tailwind installed, header added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+
+
+const Header = () => {
+    return (
+        <header className="flex justify-between items-center">
+            <div className="w-16">
+                <img src="https://i.ibb.co.com/gLngNr2Q/logo-transparent.png" alt="logo" />
+                {/* https://i.ibb.co.com/gLngNr2Q/logo-transparent.png
+                https://i.ibb.co.com/SXMnFKY0/logo-transparent-with.png */}
+            </div>
+            <div className="flex gap-4">
+                <li className="flex gap-4">
+                    <ul><a href="">Home</a></ul>
+                    <ul><a href="">Available</a></ul>     
+                    <ul><a href="">Events</a></ul>
+                    <ul><a href="">Schedules</a></ul> 
+                </li>
+                <button className="border-2 border-blue-800 px-1 rounded-md">Coin</button>
+            </div>
+            
+        </header>
+    );
+};
+
+export default Header;
