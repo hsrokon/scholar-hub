0134552e2709d7be726ea2533340795b1380b329
hsrokon <hsrokonofficial@gmail.com>
2025-04-19 09:13:35 +0600

    Conditional available & selected component rendering through it's side by side button

--- src/components/scholars/Scholars.jsx
+++ src/components/scholars/Scholars.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import Scholar from "../scholar/Scholar";
+import Selected from "../selected/Selected";
 
 const Scholars = () => {
 
     const [scholars, setScholars] = useState([]);
 
+    const [activeTab, setActiveTab] = useState('available');
+    
     useEffect(()=>{
         fetch('data.json')
         .then(res => res.json())
@@ -13,19 +16,28 @@ const Scholars = () => {
     return (
         <div className="mt-20 lg:my-10">
             <div className="flex flex-col gap-1.5 lg:gap-0 lg:flex-row items-center justify-between my-8">
-                <h2 className="text-lg lg:text-2xl font-bold">Available scholars : ({scholars.length})</h2>
+                <h2 className="text-lg lg:text-2xl font-bold">
+                    {activeTab==='available'? `Available scholars : (${scholars.length})` : `Selected scholars: ()`} 
+                </h2>  
                 <div>
-                    <button className="border-2 px-2 py-1 rounded-l-lg border-sky-950 hover:bg-sky-950 hover:text-white hover:border-amber-300">Available</button>
-                    <button className="border-2 px-2 py-1 rounded-r-lg border-sky-950 hover:bg-sky-950 hover:text-white hover:border-amber-300">Selected</button>
+                    <button 
+                    className={`border-2 px-2 py-1 rounded-l-lg  ${activeTab === 'available' ? "bg-sky-950 text-white border-amber-300" : "border-sky-950"} `} 
+                    onClick={()=>setActiveTab('available')}>Available</button>
+                    <button 
+                    className={`border-2 px-2 py-1 rounded-r-lg  ${activeTab === 'selected' ? "bg-sky-950 text-white border-amber-300" : "border-sky-950"}`}
+                    onClick={()=>setActiveTab('selected')}>Selected</button>
                 </div>
             </div>
-            <div className="grid lg:grid-cols-3 gap-6 lg:gap-12">
+
+            {activeTab === 'available' ? 
+            (<div className="grid lg:grid-cols-3 gap-6 lg:gap-12">
                 {
                     scholars.map(scholar => <Scholar
                         key={scholar.id}
                         scholar={scholar}></Scholar>)
                 }
-            </div>
+            </div>) : <Selected></Selected>}
+            
         </div>
     );
 };
---
+++ src/components/selected/Selected.jsx
@@ -0,0 +1,11 @@
+
+
+const Selected = () => {
+    return (
+        <div>
+            <h1>selected goes here</h1>
+        </div>
+    );
+};
+
+export default Selected;
