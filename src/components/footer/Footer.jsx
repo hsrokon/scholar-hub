4b1807032958ded5905baaca50d911a32b12cf1e
hsrokon <hsrokonofficial@gmail.com>
2025-04-22 08:50:23 +0600

    footer finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,18 +3,33 @@ import Newsletter from "./Newsletter";
 
 const Footer = () => {
     return (
-        <div className="bg-sky-950 py-4 mt-36 ">
-            <div className="transform -translate-y-1/2 ">
+        <div className="bg-sky-950 mt-40 relative">
+            <div className="absolute left-1/2 transform -translate-y-1/2 -translate-x-1/2
+            p-3 border-2 border-amber-500 rounded-3xl">
                 <Newsletter></Newsletter>
             </div>
-            <div>
-                <div>
-                    <h4>About Us</h4>
-                    <p>We provide you to discover and invite renowned Islamic scholars for conferences, lectures, and events worldwide.</p>
+            <div className=" w-11/12 mx-auto flex justify-between pt-40 pb-6 text-slate-300">
+                <div className="w-1/3 flex flex-col gap-2">
+                    <h4 className="text-slate-200 font-semibold">About Us</h4>
+                    <p className="hover:text-slate-200">We help you to discover and invite renowned Islamic scholars for conferences, lectures, and events worldwide.</p>
                 </div>
-                <div></div>
-                <div></div>
+                <div className="flex flex-col gap-2">
+                    <h4 className="text-slate-200 font-semibold ">Quick Links</h4>
+                    <ul>
+                        <li className="hover:text-slate-200"><a href="#">&#x2022; Home</a></li>
+                        <li className="hover:text-slate-200"><a href="#">&#x2022; Services</a></li>
+                        <li className="hover:text-slate-200"><a href="#">&#x2022; About</a></li>
+                        <li className="hover:text-slate-200"><a href="#">&#x2022; Contact</a></li>
+                    </ul>
+                </div>
+                <div className="w-1/3 flex flex-col gap-2">
+                    <h4 className="text-slate-200 font-semibold">Visit site developer</h4>
+                    <a className="hover:text-slate-200" href="hsrokon.com">HS Rokon</a>
+                    <p className="hover:text-slate-200">- Discover the awesome projects and creative work by the developer behind this site.</p>
+                </div>
             </div>
+            <hr className="text-slate-500"/>
+            <p className="text-slate-400 text-center py-4">&#64;2025 Scholar Hub All Right Reserved.</p>
         </div>
     );
 };
--- src/components/footer/Newsletter.jsx
+++ src/components/footer/Newsletter.jsx
@@ -3,13 +3,13 @@
 const Newsletter = () => {
     return (
         <div className="flex flex-col gap-2  items-center bg-white
-            py-10 w-fit mx-auto px-10 border-2 border-amber-500 rounded-3xl">
+            py-14 w-fit mx-auto px-28 border-2 border-amber-500 rounded-3xl">
             <h2 className="text-2xl font-semibold">Subscribe to our Newsletter</h2>
             <p>Get the latest updates and news right in your inbox!</p>
             <form className="flex gap-2">
-                <input className="border border-gray-300 rounded-2xl px-4 py-1.5 placeholder:text-sm" 
+                <input className="border border-slate-400 rounded-2xl px-4 py-1.5 placeholder:text-sm" 
                 type="email" name="" id="" placeholder="Enter your email"/>
-                <button>Subscribe</button>
+                <button className="cursor-pointer bg-sky-950 text-white rounded-2xl px-3 ">Subscribe</button>
             </form>
         </div>
     );
