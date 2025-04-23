aa653aaadfd5c4ccef0e09b7e614b686dda92952
hsrokon <hsrokonofficial@gmail.com>
2025-04-23 22:15:02 +0600

    A little responsiveness fixed and full website responsiveness confirmed

--- src/components/selectedScholars/SelectedScholars.jsx
+++ src/components/selectedScholars/SelectedScholars.jsx
@@ -8,7 +8,7 @@ const SelectedScholars = ({selected, setActiveTab, handleDelete}) => {
             {
                 selected.length ===0 ? (
                 <div className="flex flex-col justify-center items-center gap-4">
-                    <h2 className="text-3xl  font-bold">No scholars selected.</h2>
+                    <h2 className="text-xl md:text-3xl font-bold">No scholars selected.</h2>
                     <HiOutlineCursorClick className="size-18"/>
                 </div>) : ''
             }
@@ -21,7 +21,10 @@ const SelectedScholars = ({selected, setActiveTab, handleDelete}) => {
                     ></SelectedScholar>)
                 }
             </div>
-            <button className="border-2 my-4 border-sky-950 bg-sky-950 hover:bg-sky-900 hover:border-amber-300 px-3 py-2 rounded-2xl text-lg text-white" onClick={()=>setActiveTab('available')}>Add scholars</button>
+            <div className="flex justify-center">
+                <button className="border-2 my-4 border-sky-950 bg-sky-950 hover:bg-sky-900 hover:border-amber-300 px-3 py-1 lg:py-2 rounded-lg lg:rounded-2xl text-base lg:text-lg text-white" onClick={()=>setActiveTab('available')}>Add scholars</button>
+            </div>
+            
         </div>
     );
 };
